667c5d0a78c0187e188c6dcf7c34e36b0b587a62
Matthew DeLio <mdelio@gmail.com>
2015-09-28 14:41:16 -0500

    Start Playing with ggplot.

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -1,29 +1,41 @@
-#test comment
-
+##### HW1
+#
+# Setup ----
+#
 rm(list = ls())
 
 library(ggplot2)
 library(kknn)
 library(cvTools)
+library(reshape2)
 
 set.seed(926)
 
-# Read data
-
+#
+# Read data ----
+#
 cars <- read.csv("../data/susedcars.csv")
 
 # Visualize
+lin <- glm(price ~ mileage, data = cars)
 
-g <- ggplot(data = cars, aes(x = mileage, y = price)) + geom_point()
-
+# # Reformat linear coefficients for plotting (there's probably an easier way)
+# # mileage=mx+b
+# # 1/m*Y +(-b/m) = X
+# # (y-b)/m = x
+# linCoefs <- data.frame("intercept" = -lin$coefficients[1]/lin$coefficients[2],
+#                        "slope" = 1/lin$coefficients[2])
+# 
+g <- ggplot() + geom_point(data=cars, aes(x=mileage, y=price)) +
+        geom_abline(aes(intercept = lin$coefficients[1],
+                        slope = lin$coefficients[2]), color="red", size=1.25) + 
+        labs(x="Mileage", y="Price [$]") +
+        ggtitle("Linear Regression of Price on Mileage")
 print(g)
 
-# Fit linear relationship
-
-lin <- glm(mileage ~ price, data = cars)
-
-# Fit knn
-
+#
+# Knn (No CV) ----
+#
 nTrain <- round(0.9 * nrow(cars))
 tInd <- sample(1:nrow(cars), nTrain)
 
@@ -31,17 +43,29 @@ cars.train <- cars[tInd, ]
 cars.test <- cars[-tInd, ]
 
 # estimate RMSE for all possible values of k
+nearestNeighbors <- 2:100
 
-outRMSE <- sapply(2:100, function(k) {
-  print(k)
+out <- data.frame(nn = nearestNeighbors,
+                  kknnRmse = rep_len(NA, length(nearestNeighbors)),
+                  cvRmse = rep_len(NA, length(nearestNeighbors)))
+
+out$kknnRmse <- sapply(out$nn, function(k) {
+  cat(sprintf("%d,", k))
   kn <- kknn(price ~ mileage, train = cars.train, test = cars.test, k = k)
   rmse <- sqrt(mean((cars.test$price - kn$fitted.values)^2))
   return(rmse)
 })
+cat("done.\n")
 
-plot(outRMSE)
-kMin <- which.min(outRMSE)
+print(ggplot(data=out, aes(nn, kknnRmse)) + geom_point() + geom_line() +
+      labs(x="Nearest Neighbors", y="RMSE"))
 
+print(sprintf("kknn Min RMSE=%3.2f (@%d nearest neighbors)", 
+              min(out$kknnRmse), out$nn[which.min(out$kknnRmse)]))
+
+#
+# KNN (CV) ----
+#
 # TODO: n-fold cross validation (would be the "right" way to do this)
 # code should get cleaned up, variable names follow no convetion, plots are ugly as hell
 
@@ -56,9 +80,8 @@ nfolds <- 10
 
 folds <- cvFolds(n = nrow(cars), K = nfolds)
 
-knn.cverr <- sapply(1:100, function(k) {
-
-  print(k)
+out$cvRmse <- sapply(out$nn, function(k) {
+  cat(sprintf("%d,", k))
   
   # calculate CV error
   cvErr <- sapply(1:nfolds, function(f) {
@@ -77,9 +100,12 @@ knn.cverr <- sapply(1:100, function(k) {
   return(mean(cvErr))
   
 })
+cat("done.\n")
 
 # TODO: should get cleaned up and put in ggplot2
-plot(outRMSE)
-plot(knn.cverr)
 
-which.min(knn.cverr) # k = 40 --> minimum cross-validated error
+outMelted <- melt(out, id.vars = "nn")
+g <- ggplot(data=outMelted, 
+            aes(x=nn, y=value, group=variable, color=factor(variable))) + 
+  geom_point() + geom_line() + labs(x="# of Nearest Neighbors", y="RMSE")
+plot(g)
