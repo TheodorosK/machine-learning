25af313eab93052ea722ab739462a336fe886f8d
Will Clark <wclark3@gmail.com>
2015-09-28 10:00:23 -0500

    added cross-validation to hw1

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -2,8 +2,7 @@ rm(list = ls())
 
 library(ggplot2)
 library(kknn)
-library(tictoc)
-library(parallel)
+library(cvTools)
 
 set.seed(926)
 
@@ -31,16 +30,54 @@ cars.test <- cars[-tInd, ]
 
 # estimate RMSE for all possible values of k
 
-tic()
 outRMSE <- sapply(2:100, function(k) {
   print(k)
   kn <- kknn(price ~ mileage, train = cars.train, test = cars.test, k = k)
   rmse <- sqrt(mean((cars.test$price - kn$fitted.values)^2))
   return(rmse)
 })
-toc()
 
 plot(outRMSE)
 kMin <- which.min(outRMSE)
 
 # TODO: n-fold cross validation (would be the "right" way to do this)
+# code should get cleaned up, variable names follow no convetion, plots are ugly as hell
+
+# This is the packaged way to do it, but the documentation is terrible
+# "cv.kknn performs k-fold crossvalidation and is generally slower and does not
+# yet contain the test of different models yet"
+kn.cv <- cv.kknn(price ~ mileage, data = cars, kcv = 10)
+
+# Home-made way to do k-fold cross validation
+
+nfolds <- 10
+
+folds <- cvFolds(n = nrow(cars), K = nfolds)
+
+knn.cverr <- sapply(1:100, function(k) {
+
+  print(k)
+  
+  # calculate CV error
+  cvErr <- sapply(1:nfolds, function(f) {
+    
+    testInd <- folds$subsets[folds$which == f]
+    trainInd <- folds$subsets[folds$which != f]
+    
+    cars.test <- cars[testInd, ]
+    cars.train <- cars[trainInd, ]
+    
+    kn <- kknn(price ~ mileage, train = cars.train, test = cars.test, k = k)
+    rmse <- sqrt(mean((cars.test$price - kn$fitted.values)^2))
+    return(rmse)
+  })
+  
+  return(mean(cvErr))
+  
+})
+
+# TODO: should get cleaned up and put in ggplot2
+plot(outRMSE)
+plot(knn.cverr)
+
+which.min(knn.cverr) # k = 40 --> minimum cross-validated error
