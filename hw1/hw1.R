0c17239d32822572306ed9d363b66662e5149f0f
Will Clark <wclark3@gmail.com>
2015-09-30 13:08:22 -0500

    linear model errors plot

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -179,3 +179,19 @@ lin.pred <- predict(lin, data.frame(mileage=100e3))
 kCVmin <- algMins$nn[algMins$algo == "cv"]
 knn.pred <- kknn(price ~ mileage, train = cars, test = data.frame(mileage=100e3), k = kCVmin)
 # predicted price: kn.pred$fitted.values ($17,936.67)
+
+##
+## Error distribution
+##
+
+epsilon <- data.frame(mileage = cars$mileage, price = cars$price, error = lin$residuals)
+# epsilon <- epsilon[order(epsilon$price, decreasing = T), ]
+
+g <- ggplot() + geom_point(data = epsilon, aes(x = mileage/1000, y = error/1000)) + 
+  labs(x = "Mileage [1000 miles]", y = "Linear Model Residual [1000 $]") + 
+  ggtitle("Distribution of Linear Model Residuals by Car Mileage") + 
+  geom_hline(yintercept = 0, lty = "dashed") + theme_bw()
+
+print(g)
+
+# plot(epsilon$error)
