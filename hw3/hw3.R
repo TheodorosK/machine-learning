70303aca5115d7e7553d42bbb7f3209e1393fad0
Will Clark <wclark3@gmail.com>
2015-10-12 12:23:51 -0500

    start experimenting with boosting

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -120,6 +120,88 @@ cat("\n\n-----random forest-----\n\n")
 rf.small <- randomForest(price ~ mileage, data = cars.test, do.trace = 20)
 rf.big <- randomForest(price ~ ., data = cars.test, do.trace = 20)
 
+# Boosting --------------------------------------------------------------------
+# play with interaction.depth, n.trees, shrinkage
+# some notes:
+# -- n.trees optimal value (for big tree) is around 41; for boost.small there 
+#    is lots of run-to-run variation however, not sure what the cause is (don't 
+#    understand boosting that well I guess)
+# -- interaction.depth optimal value around 9, w/ some r2r variation again
+# -- shrinkage optimal around 0.2, but lots of r2r variation
+# -- so far I'm assuming that there is no interaction effect of tuning params
+#    to really test this, need to simulate in 3d space, right?
+
+TestGBM <- function(export.list, test.param, test.range) {
+  sfInit(cpus = detectCores(), parallel = T)
+  if (sfParallel()) { 
+    sfRemoveAll()
+    sfExport(list=export.list)
+  }
+  sfLibrary(gbm)
+  rmse.boost <- sfClusterApplyLB(1:length(test.range), function(i) {
+    if (test.param == "n.trees") {
+      boost <- gbm(formula = price ~ ., distribution = "gaussian", 
+                   data = cars.test, n.trees = test.range[i], 
+                   interaction.depth = boost.indepth,
+                   shrinkage = boost.shrink) 
+    } else if (test.param == "interaction.depth") {
+      boost <- gbm(formula = price ~ ., distribution = "gaussian", 
+                   data = cars.test, n.trees = boost.ntree, 
+                   interaction.depth = test.range[i],
+                   shrinkage = boost.shrink) 
+    } else if (test.param == "shrinkage") {
+      boost <- gbm(formula = price ~ ., distribution = "gaussian", 
+                   data = cars.test, n.trees = boost.ntree, 
+                   interaction.depth = boost.indepth,
+                   shrinkage = test.range[i]) 
+    } else {
+      stop("That is not a valid test parameter")
+    } 
+    price.hat <- predict(boost, newdata = cars.val, n.trees = boost.ntree)
+    return(mean(sqrt((cars.val$price - price.hat)^2)))
+  })
+  cat("done\n")
+  sfStop()
+  return(unlist(rmse.boost))    
+}
+
+boost.ntree = 70
+boost.indepth = 9
+boost.shrink = 0.16
+
+test.ntree <- seq(30, 100, 10)
+test.indepth <- seq(5, 15)
+test.shrink <- seq(0.01, 0.5, 0.02)
+export.list = c("cars.test", "cars.val", "test.ntree", "test.indepth", "test.shrink",
+                "boost.ntree", "boost.indepth", "boost.shrink")
+
+rmse.boost <- TestGBM(export.list, "n.trees", test.ntree)
+cat(sprintf("minimum rmse when using n.tree = %d", 
+            test.ntree[which.min(rmse.boost)]))
+
+# Try to smooth out the run-to-run variation (this takes a while)
+# Q: is it wrong to parallelize one layer below this? 
+T <- 100
+opt.ntree <- rep(NA, 10)
+for (i in 1:T) {
+  cat(sprintf("\n\n- - - - %d - - - -\n\n", i))
+  rmse.boost <- TestGBM(export.list, "n.trees", test.ntree)
+  opt.ntree[i] <- test.ntree[which.min(rmse.boost)]
+}
+cat("done\n")
+
+# Now with the "tuned" parameters
+
+boost.small <- gbm(formula = price ~ mileage, distribution = "gaussian", 
+                   data = cars.test, n.trees = boost.ntree, 
+                   interaction.depth = boost.indepth,
+                   shrinkage = boost.shrink, verbose = T)
+
+boost.big <- gbm(formula = price ~ ., distribution = "gaussian", 
+                 data = cars.test, n.trees = boost.ntree, 
+                 interaction.depth = boost.indepth,
+                 shrinkage = boost.shrink, verbose = T)
+
 # Multiple Regression ----
 require(gamlr)
 
@@ -148,5 +230,16 @@ predict.val = data.frame(
   bag.big = rowMeans(bs.pred.big),
   rf.small = predict(rf.small, newdata = cars.val),
   rf.big = predict(rf.big, newdata = cars.val),
-  lin.reg = lin.reg
+  lin.reg = lin.reg, 
+  boost.small = predict(boost.small, newdata = cars.val, n.trees = boost.ntree),
+  boost.big = predict(boost.big, newdata = cars.val, n.trees = boost.ntree)
 )
+
+rmse.val <- sapply(1:ncol(predict.val), function(c) {
+  mean(sqrt((cars.val$price - predict.val[, c])^2))
+})
+names(rmse.val) <- names(predict.val)
+
+cat("best model:", names(predict.val)[which.min(rmse.val)])
+
+print(rmse.val[order(rmse.val)])
