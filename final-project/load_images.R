3fe14effe67d3ed8dd8a128e85d1a8f04fa88ec2
Matthew DeLio <mdelio@users.noreply.github.com>
2015-11-07 10:32:38 -0600

    Add some basic image processing functions, and exploit them for edge detection.

---
+++ final-project/load_images.R
@@ -0,0 +1,64 @@
+rm(list = ls())
+
+source("../utils/source_me.R", chdir = T)
+CreateDefaultPlotOpts()
+
+require(doSNOW)
+require(snowfall)
+require(parallel)
+
+if (!sfIsRunning()) {
+  sfInit(cpus=detectCores(), parallel = T)
+  registerDoSNOW(sfGetCluster())
+}
+
+# We seem to use this color palette everywhere, just define it once
+im.col <- gray((0:255)/255)
+
+# Load Data ###################################################################
+dat.raw <- LoadCacheTagOrRun("raw", read.csv, "data/training.csv",
+                             stringsAsFactors=F)
+im.raw <- LoadCacheTagOrRun(
+  "raw_im", sfLapply,
+  dat.raw[,"Image"], function(x) {
+    return(matrix(rev(as.integer(unlist(strsplit(x, " ")))), 96, 96))
+  })
+dat.raw$Image <- NULL
+
+# Do a couple of sanity checks.
+print(sprintf("# of images == # of data-points: %s", 
+              length(im.raw) == nrow(dat.raw)))
+image(im.raw[[1]], col=im.col)
+image(im.raw[[2]], col=im.col)
+
+# Cleanup Images ##############################################################
+source("image_processing.R")
+
+if (sfParallel()) {
+  sfRemoveAll()
+  sfExport(list=c("im.raw", "NormalizeFilter", "NormalizeImage", "Convolve"))
+}  
+im.edge_det <- LoadCacheTagOrRun(
+  'edge_det_im', sfLapply, 
+  im.raw, function(x, out.min=0, out.max=255) {
+    # Here are some basic Edge-detection kernels:
+    # https://en.wikipedia.org/wiki/Kernel_(image_processing)
+    #
+    # I want to try this at some point (it looks cool and does a good 
+    # job rejecting noise):
+    # https://en.wikipedia.org/wiki/Canny_edge_detector
+    #
+    # filt <- matrix(c(1, 0, -1, 0, 0, 0, -1, 0, 1), 3, 3)
+    filt <- matrix(c(-1, -1, -1, -1, 8, -1, -1, -1, -1), 3, 3)
+    conv <- Convolve(x, filt)
+    # Saturate before Normalization
+    conv[conv < out.min] <- out.min
+    conv[conv > out.max] <- out.max
+    return(conv)
+  })
+
+layout(matrix(1:6, 2, 3))
+for (i in sample(1:length(im.edge_det), 3)) {
+  image(im.edge_det[[i]], col=im.col, main=i)
+  image(im.raw[[i]], col=im.col, main=i)
+}
