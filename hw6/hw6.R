4d7a10a8a046baff0b0abd00b36db630c2ef59bc
Will Clark <wclark3@gmail.com>
2015-11-11 14:29:20 -0600

    hw 6 q1+2

--- hw6/hw6.R
+++ hw6/hw6.R
@@ -24,13 +24,17 @@ rm(fileConnection)
 # Cleanup #####################################################################
 
 require("recommenderlab")
+
 # create a ratingData matrix using reviewerID, itemID, and rating
 ratingData <- as(dat[c("reviewerID", "itemID", "rating")], "realRatingMatrix")
+
 # we keep users that have rated more than 2 video games
 ratingData <- ratingData[rowCounts(ratingData) > 2,]
+
 # we will focus only on popular video games that have 
 # been rated by more than 3 times
 ratingData <- ratingData[,colCounts(ratingData) > 3]
+
 # we are left with this many users and items
 dim(ratingData)
 
@@ -44,3 +48,11 @@ as(rec, "list")
 # predict ratings 
 rec <- predict(r, ratingData[10, ], type="ratings")
 as(rec, "matrix")
+
+# Question 1: Which user has rated the most games? ############################
+
+which.max(rowCounts(ratingData))
+
+# Question 2: Which game has been rated by the most users? ####################
+
+which.max(colCounts(ratingData))
