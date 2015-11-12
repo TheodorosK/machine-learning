0591d3cdce75a4584d8be4d867c10f19e6b9a030
Matthew DeLio <mdelio@users.noreply.github.com>
2015-11-11 20:42:46 -0600

    Add top-10 exports.

--- hw6/hw6.R
+++ hw6/hw6.R
@@ -5,6 +5,7 @@ require(jsonlite)
 require(recommenderlab)
 
 source("../utils/source_me.R", chdir=T)
+CreateDefaultPlotOpts()
 
 # Load Data ###################################################################
 fileConnection <- gzcon(url("https://github.com/ChicagoBoothML/MachineLearning_Fall2015/raw/master/Programming%20Scripts/Lecture07/hw/videoGames.json.gz"))
@@ -50,21 +51,23 @@ gameMost <- which.max(colCounts(ratingData))
 colCounts(ratingData[, gameMost])
 
 # Question 3: Which user is most similar to U141954350? #######################
+user <- "U141954350"
+uIdx <- which(rownames(ratingData) == user)
 
-rdNorm <- normalize(ratingData)
-rdBin <- binarize(rdNorm, 0) # for Jaccard; split into 0/1 by below/above average
-  
-uIdx <- which(rownames(rdNorm) == "U141954350")
+simCosine <- similarity(x=ratingData[uIdx,], y=ratingData[-uIdx,],
+                        method="cosine", which="users")
+top10.Cosine <- colnames(simCosine)[order(simCosine, decreasing = T)][1:10]
 
-simCosine <- similarity(x = rdNorm[uIdx, ], y = rdNorm[-uIdx, ], 
-                              method = "cosine", which = "users")
-head(simCosine[order(simCosine, decreasing = T)])
+# for Jaccard; split into 0/1 by below/above average
+rdBin <- binarize(normalize(ratingData), minRating=0) 
+simJaccard <- similarity(x=rdBin[uIdx, ], y=rdBin[-uIdx, ], 
+                         method="jaccard", which="users")
+top10.Jaccard <- colnames(simJaccard)[order(simJaccard, decreasing = T)][1:10]
 
-simJaccard <- similarity(x = rdBin[uIdx, ], y = rdBin[-uIdx, ], 
-                         method = "jaccard", which = "users")
-simJaccard[order(simJaccard, decreasing = T)]
+top10 <- data.frame(Jaccard=top10.Jaccard, Cosine=top10.Cosine)
+ExportTable(top10, "top10", sprintf("Top10 Users Similar to %s", user))
 
-
-simPearson <- similarity(x = ratingData[uIdx, ], y = ratingData[-uIdx, ], 
-                              method = "pearson", which = "users")
-head(simPearson[order(simPearson)])
+# Keep getting NAs, skip this for now.
+# simPearson <- similarity(x=ratingData[uIdx,], y=ratingData[-uIdx,],
+#                          method="pearson", which="users")
+# colnames(simPearson[order(simPearson)])
