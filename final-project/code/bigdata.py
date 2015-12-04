a83bcba972bb709e6dfb65474d8f7d5be7cea67d
Will Clark <wclark3@gmail.com>
2015-12-04 15:19:42 -0600

    rf and lasso working in script

--- final-project/code/bigdata.py
+++ final-project/code/bigdata.py
@@ -5,17 +5,18 @@ import time
 import lasagne
 import numpy as np
 import sklearn.ensemble
+import sklearn.linear_model
 
 import fileio
 import partition
 
 faces_csv = os.path.abspath("../data/training.csv")
 faces_pickled = os.path.abspath("../data/training.pkl.gz")
-num_rows = 1000
+num_rows = 500
 
 start_time = time.time()
 faces = fileio.FaceReader(faces_csv, faces_pickled,
-                          fast_nrows=500)
+                          fast_nrows=num_rows)
 faces.load_file()
 print "Read Took {:.3f}s".format(time.time() - start_time)
 
@@ -43,12 +44,17 @@ for k in partitions.keys()	:
         k, partitions[k]['X'].shape, partitions[k]['Y'].shape))
 
 num_features = raw_data['Y'].shape[1]
-err = np.empty((1,num_features))
+err_rf = np.empty((1,num_features))
+err_lin = np.empty((1,num_features))
 
 partitions['train']['X'].shape # (300, 1, 9216)
 partitions['train']['Y'].shape # (300, 30)
 
-for i in range(5): #range(partitions['train']['Y'].shape[1]):
+# Drop into a console so that we do anything additional we need.
+code.interact(local=locals())
+
+
+for i in range(1): #range(partitions['train']['Y'].shape[1]):
 	print "*"*80 + "\n"
 	print "Feature #" + str(i+1)
 
@@ -60,43 +66,23 @@ for i in range(5): #range(partitions['train']['Y'].shape[1]):
 	Y = partitions['train']['Y'][to_keep]
 	print "Dropping samples with NaNs: {:3.1f}% dropped".format(float(sum(~to_keep))/float(len(to_keep))*100.)
 
+	# Random forest
 	rf = sklearn.ensemble.RandomForestRegressor(n_estimators=10, n_jobs=-1)
 	rf = rf.fit(X, Y[:,i])
-	yhat = rf.predict(partitions['validate']['X'])
-	err[0][i] = np.sqrt(np.nanmean(np.power(yhat - partitions['validate']['Y'][:,i], 2)))
-	print "RMSE = " + str(err[0][i])
+	yhat_rf = rf.predict(partitions['validate']['X'])
+	err_rf[0][i] = np.sqrt(np.nanmean(np.power(yhat_rf - 
+		partitions['validate']['Y'][:,i], 2)))
+	print "Random Forest RMSE = " + str(err_rf[0][i])
 	print "calculation time: {:.3f}s".format(time.time() - start_time)
 
+	# Lasso
+	lin = sklearn.linear_model.LassoCV(tol=0.0005, n_jobs=-1)
+	lin = lin.fit(X, Y[:,i])
+	yhat_lin = lin.predict(partitions['validate']['X'])
+	err_lin[0][i] = np.sqrt(np.nanmean(np.power(yhat_lin - 
+		partitions['validate']['Y'][:,i], 2)))
+	print "Lasso RMSE = " + str(err_lin[0][i])
+	print "calculation time: {:.3f}s".format(time.time() - start_time)
+
+
 print "Done"
-
-# Drop into a console so that we do anything additional we need.
-code.interact(local=locals())
-
-# rf = sklearn.ensemble.RandomForestRegressor(n_jobs=-1)
-	# rf = rf.fit(partitions['train']['X'], 
-	# 	partitions['train']['Y'][:,i])
-	# yhat = rf.predict(partitions['validate']['X'])
-	# err[0][i] = np.sqrt(np.mean(np.power(yhat - partitions['validate']['Y'][:,i], 2)))
-	
-
-# X = np.ndarray((raw_data['X'].shape[0], 
-# 	raw_data['X'].shape[2]*raw_data['X'].shape[3]))
-# for i in range(raw_data['X'].shape[0]):
-# 	X[i, :] = np.ndarray.flatten(raw_data['X'][i][0])
-# Y = raw_data['Y'][:,0]
-
-# testX = X[0:80,:]
-# valX = X[80:100,:]
-# testY = Y[0:80]
-# valY = Y[80:100]
-
-# rf = sklearn.ensemble.RandomForestRegressor(n_estimators=10, 
-# 	criterion="mse", n_jobs=1, verbose=1)
-
-# rf = rf.fit(X=testX, y=testY)
-
-# out = rf.predict(valX)
-
-# err = np.sqrt(np.mean(np.power(out-valY,2)))
-# print err
-
