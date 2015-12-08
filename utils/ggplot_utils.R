eb7832a5bcff51f79755530646765125f7c98b93
Will Clark <wclark3@gmail.com>
2015-12-08 14:20:05 -0600

    add alpha to ggplot utils

--- utils/ggplot_utils.R
+++ utils/ggplot_utils.R
@@ -1,3 +1,12 @@
+# http://www.magesblog.com/2013/04/how-to-change-alpha-value-of-colours-in.html
+add.alpha <- function(col, alpha=1){
+  if(missing(col))
+    stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, 
+        function(x) 
+          rgb(x[1], x[2], x[3], alpha=alpha))  
+}
+
 # Replicates the ggplot2 color palette
 # http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
 gg_color_hue <- function(n) {
